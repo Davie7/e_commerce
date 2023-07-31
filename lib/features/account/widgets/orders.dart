560574b606517086f1f04ade3cafe2ff3d9b139b
Dave Emery <davemery2000@gmail.com>
2023-07-31 16:01:12 +0300

    small commit

--- lib/features/account/widgets/orders.dart
+++ lib/features/account/widgets/orders.dart
@@ -9,6 +9,15 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
+  //temporary list
+  List list = [
+    {
+      'https://images.unsplash.com/photo-1689711596068-8ddb46cc0b83?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfE04alZiTGJUUndzfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+      'https://images.unsplash.com/photo-1689711596068-8ddb46cc0b83?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfE04alZiTGJUUndzfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+      'https://images.unsplash.com/photo-1689711596068-8ddb46cc0b83?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfE04alZiTGJUUndzfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+      'https://images.unsplash.com/photo-1689711596068-8ddb46cc0b83?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfE04alZiTGJUUndzfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -42,7 +51,20 @@ class _OrdersState extends State<Orders> {
           ],
         ),
         // display orders
-        Container(height: 170,),
+        Container(
+          height: 170,
+          padding: const EdgeInsets.only(
+            left: 10,
+            top: 20,
+            right: 0,
+          ),
+          child: ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (BuildContext context, int index) {
+              return;
+            },
+          ),
+        ),
       ],
     );
   }
